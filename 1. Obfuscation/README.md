276103c9e4fdbf4c32898e29a7ca51ebb1fc3ed6
Kirill Stonozhenko <kirill_stonozhenko@dell.com>
2020-09-10 14:31:00 +0300

    README and alphabet checks added

---
+++ 1. Obfuscation/README.md
@@ -0,0 +1,19 @@
+# Obfuscation
+This project contains software solution of an XML-obfuscator that affects attributes values and simple texts making it unreadable.
+
+Algorithm uses simple one-character permutation cipher.
+
+Program works in two modes:
+- obfuscation
+- unobfuscation
+
+If you process file in these two modes serially, you'll get initial file.
+
+###Usage
+
+```bash
+gradle run args='[obfuscate/unobfuscate] [filepath]'
+``` 
+
+First argument stands for the mode of application.
+Second - for the path to input file.
--- 1. Obfuscation/src/main/java/Obfuscator.java
+++ 1. Obfuscation/src/main/java/Obfuscator.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Obfuscator {
@@ -9,6 +10,12 @@ public class Obfuscator {
     public Obfuscator(String source_alphabet_file, String target_alphabet_file) {
         source = readAlphabet(source_alphabet_file);
         target = readAlphabet(target_alphabet_file);
+        try {
+            checkAlphabets(source, target);
+        } catch (IOException exception){
+            System.err.println(exception.getMessage());
+            exception.printStackTrace();
+        }
     }
 
     private static String readAlphabet (String alphabet_file) {
@@ -16,15 +23,38 @@ public class Obfuscator {
         try {
             File srcFile = new File(alphabet_file);
             Scanner srcReader = new Scanner(srcFile);
-            while (srcReader.hasNextLine()) {
-                return_value = srcReader.nextLine();
-            }
+            return_value = srcReader.nextLine();
             srcReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred while reading alphabet file.");
             e.printStackTrace();
         }
         return return_value;
+    }
+
+    private static boolean checkAlphabets (String alpha1, String alpha2) throws IOException {
+        int len1 = alpha1.length();
+        int len2 = alpha2.length();
+        if (len1 != len2) {
+            String message = "Alphabets have different length: " + len1 + " and " + len2;
+            throw new IOException(message);
+        }
+        return checkAlphabetUniqueness(alpha1) && checkAlphabetUniqueness(alpha2);
+    }
+
+    private static boolean checkAlphabetUniqueness(String alphabet) throws IOException{
+        int len = alphabet.length();
+        for (int i=0; i < len; i++) {
+            char first_symbol = alphabet.charAt(i);
+            for (int j = i + 1; j < len; j++) {
+                char second_symbol = alphabet.charAt(j);
+                if (first_symbol == second_symbol) {
+                    String message = "Alphabet " + alphabet + " has not unique symbol: " + first_symbol;
+                    throw new IOException(message);
+                }
+            }
+        }
+        return true;
     }
 
     public String obfuscate(String s) {
